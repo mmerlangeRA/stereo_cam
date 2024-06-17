b5dae83068607459b61b7c910d1d5b3dd444bccb
mmerlange44 <mmerlange@gmail.com>
2024-06-17 15:18:33 +0200

    almost working

--- triangulate_360_2.py
+++ triangulate_360_2.py
@@ -2,118 +2,82 @@ import os
 import numpy as np
 import cv2 as cv
 from scipy.optimize import minimize
-from src.calibrate import calibrate_left_right
+from src.calibrate import calibrate_left_right, getCalibrationFrom3Matching, load_calibration_custom, load_calibration_params, save_calibration_custom, save_calibration_params
 from src.triangulate import rotation_matrix_from_params, triangulate_point,get_3d_point_cam1_2_from_coordinates
+import yaml
 
 
-def estimate_pos_on_screen(d1,d2, t,image_width,image_height):
-    cosA =(pow(d1,2)+pow(t,2)-pow(d2,2))/(2*d1*t)
-    cosB =(pow(d2, 2)+pow(t, 2)-pow(d1, 2))/(2*d2*t)
-    A  = np.arccos(cosA)
-    B = np.arccos(cosB)
-    print(A, B)
-    theta1 = np.pi/2.-A
-    theta2 = B - np.pi/2.
-    print(theta1, theta2)
-
-    normalizedTheta1 = theta1/np.pi
-    normalizedTheta2 = theta2/np.pi
-
-    half_width = image_width/2.
-
-    posx1= normalizedTheta1*half_width+half_width
-    posx2= normalizedTheta2*half_width+half_width
-    posx1_b= 0
-    posx2_b= 0
-    return posx1, posx2,posx1_b,posx2_b
-
-tx=1.12
-print("****************************************")
-posx1, posx2,posx1_b,posx2_b=estimate_pos_on_screen(14,14,tx, 5376,200)
-print(posx1, posx2,posx1_b,posx2_b)
-# print("****************************************")
-# posx1, posx2,posx1_b,posx2_b=estimate_pos_on_screen(14,14,tx, 5376,200)
-# print(posx1, posx2,posx1_b,posx2_b)
+ray1 = np.array([1.1, 0.1, 0.2])
+ray2 = np.array([0, 1.2, 0.1])
+identity_t = np.array([0, 0, 0])
+identity_R = np.eye(3)
 
 
-def objective_function(params, ray1, ray2, known_distance):
-    """Objective function for optimization."""
-    R_matrix = rotation_matrix_from_params(params[:3])
-    tx = params[3:][0]
-    point_3d,residuals = triangulate_point(ray1, ray2, tx, R_matrix)
-    estimated_distance = np.linalg.norm(point_3d)
-    return (estimated_distance - known_distance) ** 2
+''' "good one"
+{'max_inliers': 2232, 'R': array([[ 0.99660993,  0.01121534,  0.08150376],
+       [-0.01297537,  0.99969323,  0.02109695],
+       [-0.08124215, -0.02208297,  0.99644973]]), 't': array([ 1.08000034, -0.01266376, -0.05783629])}
+[4.8285099  0.44539063 7.78940202] [4.38051853 0.57482834 7.50472628] [6.35491574e-06]
+[ 4.80901366 -0.30651955  7.74210788] [ 4.3488008  -0.17759597  7.47578838] [9.73542891e-06]
+size 0.7536482982240809
+'''
 
-def global_objective_function(params, data):
-    target =0
-    R_matrix = rotation_matrix_from_params(params[:3])
-    tx = params[3:][0]
-    #for d in data:
-    for d in data:
-        if not d["valid"]:
-            continue
-        image_width = d["image_width"]
-        image_height = d["image_height"]
-        d["keypoints"][0], d["keypoints"][1]
-        point_image1 = np.array( d["keypoints"][0]) 
-        point_image2 = np.array(d["keypoints"][1]) 
-        known_distance_1 = d["distances"][0]
-        known_distance_2 = d["distances"][1]
-        theta1, phi1 = pixel_to_spherical_revised(image_width, image_height, point_image1[0], point_image1[1])
-        theta2, phi2 = pixel_to_spherical_revised(image_width, image_height, point_image2[0], point_image2[1])
-        ray1 = spherical_to_cartesian(theta1, phi1)
-        ray2 = spherical_to_cartesian(theta2, phi2)
-        point_3d_1,residuals = triangulate_point(ray1, ray2, tx,R_matrix)
-        distance_cam1_to_point = np.linalg.norm(point_3d_1)
-        point_3d_cam2 = R_matrix @ (point_3d_1 - [0.,tx,0.])  
-        distance_cam2_to_point = np.linalg.norm(point_3d_cam2)
-        target += (distance_cam1_to_point - known_distance_1) ** 2
-        target += (distance_cam2_to_point - known_distance_2) ** 2
-    return target
-
+'''
+R [[ 0.99986244  0.00424974 -0.01603238]
+ [-0.00395075  0.99981855  0.01863503]
+ [ 0.01610867 -0.01856913  0.9996978 ]]
+t [ 1.08        0.01091257 -0.1       ]
+'''
 
 data=[
-        {#P1
-            "keypoints": [[537, 1303],[525, 1292]],
+        {#P1_0
+            "id":"P1_0",
+            "keypoints_top": [[522, 1278],[512, 1267]],
+            "keypoints_bottom": [[522, 1327],[509, 1315]],
             "distances": [13.8,14.5],
             "image_width": 5376,
             "image_height": 2688,
-            "valid":False
+            "valid":True
         },
-        {
-            "keypoints": [[823, 1296],[806, 1292]],
+        {#P1_1
+            "id":"P1_1",
+            "keypoints_top": [[803, 1258],[790, 1258]],
+            "keypoints_bottom": [[801, 1330],[786, 1323]],
             "distances": [9.57,10.6],
             "image_width": 5376,
             "image_height": 2688,
-            "valid":False
+            "valid":True
         },
         {#P1_2
-
-            "keypoints": [[3206, 1223],[3046, 1333]],
-            "distances": [9.68,9.05],#probably mistake here, so I switched, obviously panneau à droite
+            "id":"P1_2",
+            "keypoints_top": [[3196, 1297],[3034, 1305]],
+            "keypoints_bottom": [[3196, 1367],[3035, 1379]],
+            "distances": [9.68,9.05],
             "image_width": 5376,
             "image_height": 2688,
             "valid":True
         },
         {#P2_2
-
-            "keypoints": [[2843, 1411],[2717, 1505]],
+            "id":"P2_2",
+            "keypoints_top": [[2824, 1405],[2672, 1492]],
+            "keypoints_bottom": [[2824, 1436],[2670, 1561]],
             "distances": [6.35,6.28],#probably mistake here, so I switched, obviously panneau à gauche
-            "image_width": 5376,
-            "image_height": 2688,
-            "valid":False
-        },
-        {#P2_1
-
-            "keypoints": [[2820, 1397],[2703, 1436]],
-            "distances": [13.85,13.77],#probably mistake here, so I switched, obviously panneau à droite
             "image_width": 5376,
             "image_height": 2688,
             "valid":True
         },
+        {#P2_1
+            "id":"P2_1",
+            "keypoints": [[2820, 1397],[2703, 1436]],
+            "distances": [13.85,13.77],#probably mistake here, so I switched, obviously panneau à droite
+            "image_width": 5376,
+            "image_height": 2688,
+            "valid":False
+        },
         {#P3_1
-
-            "keypoints": [[3043, 1316],[2887, 1389]],
+            "id":"P3_1",
+            "keypoints_top": [[3007, 1273],[2848, 1285]],
+            "keypoints_bottom": [[3075, 1272],[2922, 1284]],
             "distances": [11.08,10.6],
             "image_width": 5376,
             "image_height": 2688,
@@ -125,7 +89,7 @@ data=[
             "distances": [19.6,19.6],
             "image_width": 5376,
             "image_height": 2688,
-            "valid":True
+            "valid":False
         },
         {#P4_2
 
@@ -133,11 +97,12 @@ data=[
             "distances": [8.379,8.179],
             "image_width": 5376,
             "image_height": 2688,
-            "valid":True
+            "valid":False
         },
         {#P5_2
-
-            "keypoints": [[3066, 1283],[2886, 1293]],
+            "id":"P5_2",
+            "keypoints_top": [[3034, 1258],[2851, 1268]],
+            "keypoints_bottom": [[3035, 1329],[2853, 1342]],
             "distances": [8.8,8.35],
             "image_width": 5376,
             "image_height": 2688,
@@ -146,8 +111,8 @@ data=[
     ]
 # Main execution
 img_folder = os.path.join(os.getcwd(), 'Photos', 'P1')
-left_image_path = 'D_P1_CAM_G_2_EAC.png'
-right_image_path = 'D_P1_CAM_D_2_EAC.png'
+left_image_path = 'D_P1_CAM_G_1_EAC.png'
+right_image_path = 'D_P1_CAM_D_1_EAC.png'
 
 left_image = cv.imread(os.path.join(img_folder, left_image_path))
 right_image = cv.imread(os.path.join(img_folder, right_image_path))
@@ -155,113 +120,122 @@ right_image = cv.imread(os.path.join(img_folder, right_image_path))
 # Image dimensions
 image_height,image_width = left_image.shape[:2]
 
-#top right of T
-point_image1 = np.array([3206, 1223])  # Keypoints in image from Camera 1 (left)
-point_image2 = np.array([3046, 1333])  # Keypoints in image from Camera 2 (right)
+angle_max = np.pi*10./180.
+dt_max = 0.12001
+bnds = ((-angle_max, angle_max), (-angle_max, angle_max),(-angle_max, angle_max),(1.11, 1.13),(-dt_max,dt_max),(-dt_max,dt_max))
+initial_params = [0, 0, 0, 1.12,0,0]
+initial_params_0 = [0, 0, 0, 0.0,0,0]
 
-# Relative position and rotation (example values)
-t = [1.12,0,0]  # Example translation: 1.12 meters along the x-axis
-R = np.eye(3)  # Assuming no rotation for simplicity
+identity_t = np.array([0, 0, 0])
+identity_R = np.eye(3)
 
-# Triangulate the 3D point
-p1,p2,_=get_3d_point_cam1_2_from_coordinates(point_image1, point_image2, image_width, image_height, R, t)
-print("p1", p1,np.linalg.norm(p1))
-print("p2", p2,np.linalg.norm(p2))
+initial_t=np.array([0.12, 0, 0])
 
-best_results  = calibrate_left_right(left_image,right_image)
-print(best_results)
-
-'''
-p1,p2=get_3d_point_cam1_2_from_coordinates(point_image1, point_image2, image_width, image_height, R, tx+0.01)
-print("p1", p1,np.linalg.norm(p1))
-print("p2", p2,np.linalg.norm(p2))
-#1cm => 5 cm
+''' for P1_2
+optimized_R [[ 9.99999992e-01 -1.25840390e-04  1.97564767e-05]
+ [ 1.25841868e-04  9.99999989e-01 -7.48031043e-05]
+ [-1.97470632e-05  7.48055899e-05  9.99999997e-01]]
+R [[ 9.99990405e-01  4.37535532e-03 -2.15538834e-04]
+ [-4.37556379e-03  9.99989950e-01 -9.76420750e-04]
+ [ 2.11264480e-04  9.77354486e-04  9.99999500e-01]]
+t [ 1.11911477  0.09601804 -0.12001   ]
 '''
 
-if False:
-    average = 0
-    for d in data:
-        print("#####Checking########")
-        point_image1 = np.array(d["keypoints"][0])
-        point_image2 = np.array(d["keypoints"][1])
-        d1=d["distances"][0]
-        d2 =d["distances"][1]
-        image_width = d["image_width"]
-        image_height = d["image_height"]
-        posx1,posx2,posx1_b,posx2_b = estimate_pos_on_screen(d1,d2, tx,image_width,image_height)
+'''for P1_1
+R [[ 9.99989311e-01  4.61184898e-03 -3.29632740e-04]
+ [-4.60813155e-03  9.99934190e-01  1.05061928e-02]
+ [ 3.78064021e-04 -1.05045615e-02  9.99944754e-01]]
+t [1.11    0.12001 0.12001]
 
-        minx1 = min(abs(posx1-point_image1[0]),abs(posx1_b-point_image1[0]))
-        minx2 = min(abs(posx2-point_image2[0]),abs(posx2_b-point_image2[0]))
-        m = max(minx1, minx2)
-        average +=m
-        if m > 50:
-            print("expected",point_image1[0],point_image2[0])
-            print(f"from {d1},{d2},{tx}")
-            print(posx1,posx2,posx1_b,posx2_b )
-            print("error?", m)
-        print(point_image1,point_image2)
-    print("average", average/len(data))
+'''
 
-if False:
-    angle_max = np.pi*7./180.
-    bnds = ((-angle_max, angle_max), (-angle_max, angle_max),(-angle_max, angle_max),(1.05, 1.19))
+#point_3d, residuals = triangulate_point(ray1, ray2, t, R, verbose=True)
+#print("3D coordinates of the point:", point_3d)
+#print("Residuals:", residuals)
+sub_uv1 = [[3196, 1297]]
+sub_uv2 = [[3034, 1305]]
 
-    # Optimize for the rotation parameters
-    print("######### Optimize 1############")
-    initial_params = [0, 0, 0, 1.12]
-    known_distance = 9.68
-    result = minimize(objective_function, initial_params, args=(ray1, ray2, known_distance), method='BFGS')
+print("********If identity******")
+p1_top,p2_top,residual_distance_normalized = get_3d_point_cam1_2_from_coordinates(sub_uv1[0], sub_uv2[0], image_width, image_height, identity_R, initial_t,True)
+print(p1_top,p2_top,residual_distance_normalized)
+print("\n")
 
-    # Extract the optimized rotation matrix
-    optimized_params = result.x
-    optimized_R = rotation_matrix_from_params(optimized_params[:3])
-    optimized_tx = optimized_params[3:][0]
-    print("optimized_tx",optimized_tx)
+print("********If hard coded ******")
+optimized_R_s = np.array([[ 0.99660993,  0.01121534,  0.08150376],
+    [-0.01297537,  0.99969323,  0.02109695],
+    [-0.08124215, -0.02208297,  0.99644973]])
+optimized_t_s=np.array([ 1.08000034, -0.01266376, -0.05783629])
+print("R", optimized_R_s)
+print("t", optimized_t_s)
+p1_top,p2_top,residual_distance_normalized = get_3d_point_cam1_2_from_coordinates(sub_uv1[0], sub_uv2[0], image_width, image_height, optimized_R_s, optimized_t_s,True)
+print(p1_top,p2_top,residual_distance_normalized)
+print("\n")
 
-    point_3d,residuals = triangulate_point(ray1, ray2, optimized_tx, optimized_R)
+print("********Optimizing 1 point only******")
+optimized_params, residual_distance_normalized = getCalibrationFrom3Matching(sub_uv1, sub_uv2, initial_params,left_image.shape[1], left_image.shape[0],bnds)
+print("optimized_params", optimized_params)
+print("residual_distance_normalized", residual_distance_normalized)
+optimized_R = rotation_matrix_from_params(optimized_params[:3])
+optimized_t = optimized_params[3:]
 
-    print("Optimized rotation matrix R:\n", optimized_R)
-    euler_angles_rad = SciPyRotation.from_matrix(optimized_R).as_euler('xyz')
-    euler_angles_deg = np.degrees(euler_angles_rad)
-    print("Rotation angles (degrees):\n", euler_angles_deg)
+p1_top,p2_top,residual_distance_normalized = get_3d_point_cam1_2_from_coordinates(sub_uv1[0], sub_uv2[0], image_width, image_height, optimized_R, optimized_t,True)
+print(p1_top,p2_top,residual_distance_normalized)
 
-    print("Optimized t:\n", optimized_tx)
-    print("3D coordinates of the point:", point_3d)
-    print(f'optimized with distance {np.linalg.norm(point_3d)}')
+print("p1_top",p1_top)
+print("p2_top",p2_top)
+print(f'p1_top size {np.linalg.norm(p1_top)}')
+print(f'distance {np.linalg.norm(p1_top-p2_top)}')
 
-    print("######### Optimize Global############")
-    initial_params = [0, 0, 0, 1.12]
-    result = minimize(global_objective_function, initial_params, args=(data), bounds=bnds)
-    # Extract the optimized rotation matrix
-    optimized_params = result.x
-    optimized_R = rotation_matrix_from_params(optimized_params[:3])
-    optimized_tx = optimized_params[3:][0]
-    print("optimized_tx",optimized_tx)
+print("\n")
 
-    point_3d,residuals = triangulate_point(ray1, ray2, optimized_tx, optimized_R)
+if True:
+    regenerate = True  
+    file_name= "3_1"
+    s = file_name.split("_")
+    photo = s[0]
+    angle = s[1]
+    img_folder = os.path.join(os.getcwd(), 'Photos', 'P'+photo)
+    left_image_path = 'D_P'+photo+'_CAM_G_'+angle+'_EAC.png'
+    right_image_path = 'D_P'+photo+'_CAM_D_'+angle+'_EAC.png'
+    print(left_image_path, right_image_path)
 
-    print("Optimized rotation matrix R:\n", optimized_R)
-    euler_angles_rad = SciPyRotation.from_matrix(optimized_R).as_euler('xyz')
-    euler_angles_deg = np.degrees(euler_angles_rad)
-    print("Rotation angles (degrees):\n", euler_angles_deg)
-    print("Optimized t:\n", optimized_tx)
-    print("3D coordinates of the point:", point_3d)
-    print(f'optimized with distance {np.linalg.norm(point_3d)}')
+    left_image = cv.imread(os.path.join(img_folder, left_image_path))
+    right_image = cv.imread(os.path.join(img_folder, right_image_path))
+    print("********Full calibration******")
+    if regenerate or not os.path.exists(file_name+".npz"):
+        best_results = calibrate_left_right(left_image, right_image, initial_params, bnds)
+        optimized_R= best_results["R"]
+        optimized_t= best_results["t"]
+        save_calibration_params(best_results["params"],file_name)
+    else:
+        optimized_params = load_calibration_params(file_name)
+        optimized_R = rotation_matrix_from_params(optimized_params[:3])
+        optimized_t = optimized_params[3:]
 
-    # Calculate the position of the 3D point relative to the second camera
-    point_3d_cam2 = optimized_R @ (point_3d - optimized_tx)  # Rotate and translate the point to the second camera's coordinate system
-    distance_cam2_to_point = np.linalg.norm(point_3d_cam2)
+    print("R", optimized_R)
+    print("t", optimized_t)
+    print("\n")
+                                                             
 
-    # Output the distance
-    print("Distance from the second camera to the 3D point:", distance_cam2_to_point)
-
-if False:
-    optimized_R= best_results["R"]
-    optimized_t= best_results["t"]
+if True:
     for d in data:
         if not d["valid"]:
             continue
-        p1,p2,_ = get_3d_point_cam1_2_from_coordinates(d["keypoints"][0], d["keypoints"][1], image_width, image_height, optimized_R, optimized_t)
-        print(p1,p2)
-        print(f'{d["distances"][0]} vs {np.linalg.norm(p1)}, {d["distances"][1]} vs {np.linalg.norm(p2)}')
+        print("----- Calibrated R, T -------------------")
+        print(d["id"])
+        p1_top,p2_top,residual_distance_normalized = get_3d_point_cam1_2_from_coordinates(d["keypoints_top"][0], d["keypoints_top"][1], image_width, image_height, optimized_R, optimized_t, False)
+        print(p1_top,p2_top,residual_distance_normalized)
+        p1_bottom,p2_bottom,residual_distance_normalized = get_3d_point_cam1_2_from_coordinates(d["keypoints_bottom"][0], d["keypoints_bottom"][1], image_width, image_height, optimized_R, optimized_t)
+        print(p1_bottom,p2_bottom,residual_distance_normalized)
+        print(f'panneau size {np.linalg.norm(p1_top-p1_bottom)}')
+        print(f'{d["distances"][0]} vs {np.linalg.norm(p1_top)}, {d["distances"][1]} vs {np.linalg.norm(p2_top)}')
+        print("\n")
+        if False:
+            print("----- Hard coded R, T -------------------")
+            p1_top,p2_top,residual_distance_normalized = get_3d_point_cam1_2_from_coordinates(d["keypoints_top"][0], d["keypoints_top"][1], image_width, image_height, optimized_R_s, optimized_t_s,False)
+            print(p1_top,p2_top,residual_distance_normalized)
+            p1_bottom,p2_bottom,residual_distance_normalized = get_3d_point_cam1_2_from_coordinates(d["keypoints_bottom"][0], d["keypoints_bottom"][1], image_width, image_height, optimized_R_s, optimized_t_s)
+            print(p1_bottom,p2_bottom,residual_distance_normalized)
+            print(f'size {np.linalg.norm(p1_top-p1_bottom)}')
+            print(f'{d["distances"][0]} vs {np.linalg.norm(p1_top)}, {d["distances"][1]} vs {np.linalg.norm(p2_top)}')
 
